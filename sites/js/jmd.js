1d9d72b2df4175a1c15fe8841c461d0cec97bfc8
onlink66 <128705093+onlink66@users.noreply.github.com>
2023-04-05 22:16:39 +0800

    Update jmd.js

--- sites/js/jmd.js
+++ sites/js/jmd.js
@@ -1,5 +1,4 @@
 [
-{"name":"测试频道","url":"https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/lives/test.txt&&&https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/wallpaper/ys.png"},
 {"name":"央视频道","url":"https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/lives/ys.txt&&&https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/wallpaper/ys.png"},
 {"name":"卫视频道","url":"https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/lives/ws.txt&&&https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/wallpaper/ws.png"},
 {"name":"本地频道","url":"https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/lives/bd.txt&&&https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/wallpaper/bd.png"},
