21834a0609b176e4dd2fe6baf3ef359493cf51e8
onlink66 <128705093+onlink66@users.noreply.github.com>
2023-04-05 22:05:35 +0800

    update

--- sites/js/jmd.js
+++ sites/js/jmd.js
@@ -1,5 +1,5 @@
 [
-{"name":"TEST","url":"https://onlink66.github.io/lives/test.txt&&&https://onlink66.github.io/wallpaper/ys.png"},
+{"name":"TEST","url":"https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/lives/test.txt&&&https://ghproxy.com/https://raw.githubusercontent.com/onlink66/onlink66.github.io/main/wallpaper/ys.png"},
 {"name":"肥猫!!","url":"http://我不是.肥猫.love:63/TV/tvzb.txt&&&http://我不是.肥猫.love:63/lives/直播图5.png"},
 {"name":"范明","url":"https://ghproxy.com/raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u&&&https://api.likepoems.com/img/mc"}
 ]
